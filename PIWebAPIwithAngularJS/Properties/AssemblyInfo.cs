2514e468369c5f2e3144944394bdcabb42798d06
osimloeff <mloeff@osisoft.com>
2015-11-27 11:47:54 -0200

    First release

---
+++ PIWebAPIwithAngularJS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PIWebAPIwithAngularJS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PIWebAPIwithAngularJS")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad756561-f8d5-47d0-aea3-45b5e60f6bc3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PIWebAPIwithAngularJS/Scripts/piwebapi.js
@@ -0,0 +1,54 @@
+﻿//***************************************************************************
+//   Copyright 2015 OSIsoft, LLC.
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// ***************************************************************************
+
+'use strict'
+piWebApiApp.factory('piWebApiHttpService', ['$http', '$q', function ($http, $q) {
+
+    //This factory method works like a service in which all HTTP requests are made.
+    var serviceBase = 'https://marc-web-sql.marc.net/piwebapi/';
+
+    //Set withCredentials = true; if you need to type your credentais.
+    $http.defaults.withCredentials = true;
+    var piWebApiHttpServiceFactory = {};
+    piWebApiHttpServiceFactory.validPIServerName = function (piServerName) {
+        return $http.get(serviceBase + "dataservers?name=" + piServerName).then(function (response) {
+            return response;
+        });
+    };
+
+    piWebApiHttpServiceFactory.validPIPointName = function (piServerName, piPointName) {
+        return $http.get(serviceBase + "points?path=\\\\" + piServerName + "\\" + piPointName).then(function (response) {
+            return response;
+        });
+    };
+
+    piWebApiHttpServiceFactory.getSnapshotValue = function (webId) {
+        return $http.get(serviceBase + 'streams/' + webId + '/value').then(function (response) {
+            return response;
+        });
+    };
+
+    piWebApiHttpServiceFactory.getRecordedValues = function (webId, startTime, endTime) {
+        return $http.get(serviceBase + 'streams/' + webId + '/recorded?starttime=' + startTime + '&endtime=' + endTime).then(function (response) {
+            return response;
+        });
+    };
+
+    piWebApiHttpServiceFactory.getInterpolatedValues = function (webId, startTime, endTime, interval) {
+        return $http.get(serviceBase + 'streams/' + webId + '/interpolated?starttime=' + startTime + '&endtime=' + endTime + "&interval=" + interval).then(function (response) {
+            return response;
+        });
+    };
+
+    return piWebApiHttpServiceFactory;
+}]);
